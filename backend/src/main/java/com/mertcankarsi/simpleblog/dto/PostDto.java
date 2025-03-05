08470ee6236fbd3a7105fa3ffbd7049392616dd1
Mertcan Karsi <mertcankarsi@icloud.com>
2025-03-06 01:51:31 +0300

    feat: add validation annotations to PostDto and create PostControllerTest

--- backend/src/main/java/com/mertcankarsi/simpleblog/dto/PostDto.java
+++ backend/src/main/java/com/mertcankarsi/simpleblog/dto/PostDto.java
@@ -1,13 +1,19 @@
 package com.mertcankarsi.simpleblog.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class PostDto {
     private String referenceKey;
+    
+    @NotBlank(message = "Title cannot be empty")
     private String title;
+    
+    @NotBlank(message = "Content cannot be empty")
     private String content;
+    
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 } 
