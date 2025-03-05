718c4b4d6a39e7f8b9ca9368216aded6867bac7e
Mertcan Karsi <mertcankarsi@icloud.com>
2025-03-06 01:44:13 +0300

    refactor: simplify PostNotFoundException and remove PostControllerTest

--- backend/src/main/java/com/mertcankarsi/simpleblog/exception/PostNotFoundException.java
+++ backend/src/main/java/com/mertcankarsi/simpleblog/exception/PostNotFoundException.java
@@ -3,6 +3,6 @@ package com.mertcankarsi.simpleblog.exception;
 public class PostNotFoundException extends RuntimeException {
     
     public PostNotFoundException(String referenceKey) {
-        super("Post not found with reference key: " + referenceKey);
+        super(referenceKey);
     }
 } 
