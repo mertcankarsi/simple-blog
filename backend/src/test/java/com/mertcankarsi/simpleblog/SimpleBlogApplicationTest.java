d4e40cee467e410b2a22987e29943cac45d5d854
Mertcan Karsi <mertcankarsi@icloud.com>
2025-03-06 21:40:39 +0300

    refactor: rename SimpleBlogApplicationTests to SimpleBlogApplicationTest

--- backend/src/test/java/com/mertcankarsi/simpleblog/SimpleBlogApplicationTest.java
+++ backend/src/test/java/com/mertcankarsi/simpleblog/SimpleBlogApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class SimpleBlogApplicationTests {
+class SimpleBlogApplicationTest {
 
     @Test
     void contextLoads() {
