df05d9c6ef3bc10e9cbb9d0988590295ac7ff613
Mertcan Karsi <mertcankarsi@icloud.com>
2025-03-06 20:53:08 +0300

    test fix

--- backend/src/main/java/com/mertcankarsi/simpleblog/dto/PostDto.java
+++ backend/src/main/java/com/mertcankarsi/simpleblog/dto/PostDto.java
@@ -1,10 +1,15 @@
 package com.mertcankarsi.simpleblog.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostDto {
     private String referenceKey;
     
