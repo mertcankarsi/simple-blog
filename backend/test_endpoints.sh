66a2de86401af243153c63ee991b44b669b0ab74
Mertcan Karsi <mertcankarsi@icloud.com>
2025-04-20 17:04:01 +0300

    fix data initialization

--- backend/test_endpoints.sh
+++ backend/test_endpoints.sh
@@ -1,22 +1,21 @@
-
 #!/bin/bash
 
 # Test GET endpoint
 echo "Testing GET /posts"
-curl -X GET http://localhost:3000/posts
+curl -X GET http://localhost:8080/api/posts
 echo -e "\n"
 
 # Test POST endpoint
 echo "Testing POST /posts"
-curl -X POST http://localhost:3000/posts -H "Content-Type: application/json" -d '{"title": "New Post", "content": "This is the content of the new post."}'
+curl -X POST http://localhost:8080/posts -H "Content-Type: application/json" -d '{"title": "New Post", "content": "This is the content of the new post."}'
 echo -e "\n"
 
 # Test PUT endpoint
 echo "Testing PUT /posts/1"
-curl -X PUT http://localhost:3000/posts/1 -H "Content-Type: application/json" -d '{"title": "Updated Post", "content": "This is the updated content of the post."}'
+curl -X PUT http://localhost:8080/posts/1 -H "Content-Type: application/json" -d '{"title": "Updated Post", "content": "This is the updated content of the post."}'
 echo -e "\n"
 
 # Test DELETE endpoint
 echo "Testing DELETE /posts/1"
-curl -X DELETE http://localhost:3000/posts/1
+curl -X DELETE http://localhost:8080/posts/1
 echo -e "\n"
